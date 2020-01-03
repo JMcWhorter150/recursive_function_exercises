22c95c74774b2d45d204c62d6b2df9cd98ddc5bd
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-01-03 13:14:24 -0500

    Adds recursive search function that searches arrays and objs

--- exercise1.js
+++ exercise1.js
@@ -23,3 +23,69 @@ function factorial(num) {
 }
 
 // console.log(factorial(5));
+
+function productOfArray(arr) {
+    let product = arr.pop();
+    if (arr.length === 0) return 1;
+    return product * productOfArray(arr);
+}
+// console.log(productOfArray([1, 2, 3]));
+
+let nestedObject = { data: {
+    info: {
+        stuff: {
+            thing: {
+                moreStuff: {
+                    magicNumber: 44,
+                    something: 'foo2'
+                }
+            }
+        },
+        morestuff: {
+            thing: {
+                moreStuff2: {
+                    magicNumber: 445,
+                    something: 'foobar'
+                }
+            }
+        },
+        evenmore: {
+            thing: {
+                moreStuff2: {
+                    magicNumber: [9,6,7,5000],
+                    something: 'foobar'
+                }
+            }
+        },
+    }
+}
+}
+
+function contains(obj, search) {
+    if (typeof obj === 'object') {
+        for (let key of Object.keys(obj)) {
+            if (search === obj[key]) {
+                return true;
+            } else if (typeof obj[key] === 'object' || Array.isArray(obj[key])) {
+                return contains(obj[key], search);
+            } else {
+                return false;
+            }
+        }
+    } else if (Array.isArray(obj)) {
+        for (let item of obj) {
+            if (search === item) {
+                return true;
+            } else if (typeof item === 'object' || Array.isArray(item)) {
+                return contains(item, search);
+            } else {
+                return false;
+            }
+        }
+    }
+}
+
+let hasIt = contains(nestedObject, 44); // true
+let doesntHaveIt = contains(nestedObject, "foo"); // false
+console.log(hasIt);
+console.log(doesntHaveIt);
