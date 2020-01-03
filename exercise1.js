8b8cb016d3c968cb2c8e3ab26e994fc8dde3b4c1
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-01-03 11:15:30 -0500

    Adds factorial and sumRange recursive functions

--- exercise1.js
+++ exercise1.js
@@ -5,4 +5,21 @@ function countdown (num, stopNum) {
     countdown(num, stopNum);
 }
 
-countdown(7, 0);
+// countdown(7, 0);
+
+function sumRange(num) {
+    if (num <= 0) {return 0};
+    // console.log(num);
+    let newNum = num - 1;
+    return num + sumRange(newNum);
+}
+
+// console.log(sumRange(3));
+
+function factorial(num) {
+    if (num <= 1) {return 1};
+    let newNum = num - 1;
+    return num * factorial(newNum);
+}
+
+// console.log(factorial(5));
